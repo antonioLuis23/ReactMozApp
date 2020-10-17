616a58976b4fe113d78537971c15291dbafbd2ab
antonioLuis23 <antonio_luis@outlook.com>
2020-10-16 21:56:15 -0300

    fixing bug while toggling active

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-ReactDOM.render( <App subject="Antonio" />, document.getElementById('root')
+const DATA = [
+  {id: "todo-0", name: "Eat", completed: true},
+  {id: "todo-1", name: "Sleep", compelted: false},
+  {id: "todo-2", name: "Repeat", completed: false}
+]
+ReactDOM.render( <App tasks={DATA} />, document.getElementById('root')
 );
