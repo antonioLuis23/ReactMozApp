1a2ddd54dd24d120cecd14284451092c2fbb7e1f
antonioLuis23 <antonio_luis@outlook.com>
2020-10-17 00:05:58 -0300

    Adding git functionality

--- src/App.js
+++ src/App.js
@@ -27,14 +27,25 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
+  function editTask(id, newName){
+    const editedTaskList = tasks.map( task => {
+      if (id === task.id){
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   const taskList = tasks.map(task => (
     <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
+      id = {task.id}
+      name = {task.name}
+      completed = {task.completed}
+      key = {task.id}
       toggleTaskCompleted = {toggleTaskCompleted}
       deleteTask = {deleteTask}
+      editTask = {editTask}
      />
 
   ));
