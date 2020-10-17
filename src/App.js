669bf6b85fbf1728f925f87eedb655e9ad1b0d38
antonioLuis23 <antonio_luis@outlook.com>
2020-10-17 00:57:53 -0300

    adding filter option

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import {nanoid} from 'nanoid';
+
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
 
   function addTask(name){
     const newTask = {id: "todo-" + nanoid(), name: name, completed: false };
@@ -37,7 +49,9 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo
       id = {task.id}
       name = {task.name}
@@ -47,18 +61,24 @@ function App(props) {
       deleteTask = {deleteTask}
       editTask = {editTask}
      />
+  ));
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key = {name}
+      name = {name}
+      isPressed = {name === filter}
+      setFilter = {setFilter}
+     />
 
   ));
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
-  const headingText = `${taskList.length} ${tasksNoun} remaing;`
+  const headingText = `${taskList.length} ${tasksNoun} remaining;`
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton name = "All" />
-        <FilterButton name = "Active" />
-        <FilterButton name = "Completed"/>
+        {filterList}
       </div>
       <h2 id="list-heading"> {headingText} </h2>
       <ul
